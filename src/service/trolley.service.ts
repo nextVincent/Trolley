8d29a44b8c3f385679f26cd6d961c3291f38b43b
Vincent Liang <vincent.liangwx@gmail.com>
2021-06-07 09:09:10 +1000

    comment

--- src/service/trolley.service.ts
+++ src/service/trolley.service.ts
@@ -10,10 +10,12 @@ const getTrolleyTotal = async (dto: any): Promise<number> => {
  * With currenct calculator behaviour, best guess following logic should appear
  * - Products[] and Quantities[] mandatory and must match on each product.name
  * - Specials[] can be empty
- * - Specials[] serves as discount rules, can be multiple
- * - Specials[] special total amount applies only if specific rule quantity 
- *      exact match with cart quantity, otherwise no special price
- * 
+ * - Specials[] serves as discount rules
+ * - Specials[] special total amount applies only if specific rule, product quantity 
+ *      less than(or equal) with cart quantity, otherwise no special price
+ * - if multiple products, only one special rule (container all/partial products) can apply once, no overlap special calculation
+ * - if single products, multiple special rule can apply
  */
+
 
 export { getTrolleyTotal };
